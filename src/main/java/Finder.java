33d1882406fa066e81cc161bcec26df4c8f4258e
beathunter <nikitasherepa@gmail.com>
2019-03-23 18:51:49 +0300

    starting work...

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -1,12 +1,33 @@
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+
 public class Finder {
 
-    public static  void hi() {
-        System.out.println("Hello!");
-    }
-
     public static void main(String[] args) {
-        hi();
+        new Finder().start(args);
     }
+
+    @Option(name = "-d", usage = "Find file in the current directory")
+    private String directoryName;
+
+    @Option(name = "-r", usage = "Find file in the current directory and in all subdirectories")
+
+    @Argument(required = true, usage = "Name of file that needed to find", index = 0)
+    private String outputName;
+
+    public void start(String[] args) throws IllegalArgumentException {
+        CmdLineParser parse = new CmdLineParser(this);
+        try {
+            parse.parseArgument(args);
+        } catch (CmdLineException exc) {
+            System.err.println(exc.getMessage());
+            System.err.println("java -jar find.jar -r -d directoryName fileThatNeedToFind");
+            parse.printUsage(System.err);
+            return;
+        }
+    }
+
 }
-
-
