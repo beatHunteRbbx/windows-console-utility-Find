e07464ae4a464df243da6034a5a1a241764b7e88
beatHunteRbbx <nikitasherepa@gmail.com>
2019-03-26 17:16:38 +0300

    continue working...

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -3,22 +3,33 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.awt.*;
+import java.io.*;
+import java.net.URISyntaxException;
 import java.nio.file.*;
 import java.util.Collection;
 
 
 public class Finder {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*new Finder().start(args);*/
         String jarPath = Finder.class.getProtectionDomain().getCodeSource().getLocation().getPath();  //возваращет путь испольняемого-jar файла
         System.out.println(jarPath);
+        File file = new File("."); //путь к текущему каталогу
+        System.out.println(file.toPath());
+        /*File file = new File("something.txt");
+        file.createNewFile();
+        Files.copy(Paths.get("something.txt"), Paths.get("src\\something2.txt"));
+        System.out.println(Files.isDirectory(Paths.get("something.txt")));
+        System.out.println(Files.isDirectory(Paths.get("src")));*/
     }
 
     @Option(name = "-d", usage = "Find file in the current directory")
     private String directoryName;
 
     @Option(name = "-r", usage = "Find file in the current directory and in all subdirectories")
+    private boolean recursive;
 
     @Argument(required = true, usage = "Name of file that needed to find", index = 0)
     private String outputName;
