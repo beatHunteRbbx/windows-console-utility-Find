9b742642395f4a69c0300b1e8ab28b9c273a9bdc
beatHunteRbbx <nikitasherepa@gmail.com>
2019-04-09 12:28:55 +0300

    add some tests and test files

--- src/test/java/FindTest.java
+++ src/test/java/FindTest.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindTest {
 
+
+
     @Test
     public void dTest1() {
         File directory = new File("src");
@@ -18,6 +20,7 @@ public class FindTest {
         List<String> list = new ArrayList<>();
         list.add("src\\main\\file12\\file.bat");
         assertEquals(list, Find.d(directory, fileName, recursive));
+        list.clear();
     }
 
     @Test
@@ -28,6 +31,25 @@ public class FindTest {
         ArrayList<String> list = new ArrayList<>();
         list.add("src\\main\\file11\\file111\\file.bat");
         list.add("src\\main\\file12\\file.bat");
+        assertEquals(list, Find.d(directory, fileName, recursive));
+    }
+
+    @Test
+    public void dTest3() {
+        File directory = new File(".");
+        String fileName = "file.bat";
+        boolean recursive = false;
+        ArrayList<String> list = new ArrayList<>();
+        list.add(".\\file.bat");
+        assertEquals(list, Find.d(directory, fileName, recursive));
+    }
+
+    @Test
+    public void dTest4() {
+        File directory = new File("src\\main");
+        String fileName = "file.bat";
+        boolean recursive = false;
+        ArrayList<String> list = new ArrayList<>();
         assertEquals(list, Find.d(directory, fileName, recursive));
     }
 }
